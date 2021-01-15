e8c0f4593529f8107f4705d07b2f8204be20b3b7
puphs <43395605+puphs@users.noreply.github.com>
2021-01-15 19:43:54 +0400

    split User model to User and UserCredentials

---
+++ backend/src/models/User.model.ts
@@ -0,0 +1,36 @@
+import { Schema, model, Document, Model, DocumentQuery } from 'mongoose';
+import ShoppingCart, { IShoppingCartModel } from './ShoppingCart.model';
+import UserCredentials, { IUserCredentials, IUserCredentialsModel } from './UserCredentials.model';
+
+export interface IUser {
+	name: string;
+	credentials?: IUserCredentialsModel['_id'];
+	shoppingCart?: IShoppingCartModel['_id'];
+}
+
+export interface IUserModel extends IUser, Document {}
+
+const schema = new Schema({
+	name: { type: String, required: true },
+	shoppingCart: { type: Schema.Types.ObjectId, ref: 'ShoppingCart' },
+});
+
+const User: Model<IUserModel> = model('User', schema);
+
+export const createUser = async (data: IUser & IUserCredentials) => {
+	const { name, email, passwordHash } = data;
+	const user = new User({ name });
+	const credentials = new UserCredentials({ email, passwordHash });
+	const shoppingCart = new ShoppingCart();
+	user.credentials = credentials.id;
+	user.shoppingCart = shoppingCart.id;
+	credentials.user = user.id;
+	shoppingCart.user = user.id;
+
+	await credentials.save();
+	await shoppingCart.save();
+	await user.save();
+	// Model.insertMany([user, credentials, shoppingCart]);
+};
+
+export default User;
---
+++ backend/src/models/UserCredentials.model.ts
@@ -0,0 +1,18 @@
+import { Schema, model, Document, Model } from 'mongoose';
+import { IUserModel } from './User.model';
+
+export interface IUserCredentials {
+	email: string;
+	passwordHash: string;
+	user?: IUserModel['_id'];
+}
+
+export interface IUserCredentialsModel extends IUserCredentials, Document {}
+
+const schema = new Schema({
+	email: { type: String, required: true, unique: true },
+	passwordHash: { type: String, required: true },
+	user: { type: Schema.Types.ObjectId, ref: 'User' },
+});
+
+export default model('UserCredentials', schema) as Model<IUserCredentialsModel>;
