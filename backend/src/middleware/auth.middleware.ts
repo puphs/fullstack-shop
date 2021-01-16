37671760d7487f7a8c0bec5c2796f695ff54e503
puphs <43395605+puphs@users.noreply.github.com>
2021-01-16 23:08:19 +0400

    better error handling in auth.middleware

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,21 +1,24 @@
 import { DecodedTokenType, Middleware } from '../types/types';
-import jwt from 'jsonwebtoken';
+import jwt, { TokenExpiredError } from 'jsonwebtoken';
 import config from '../config/config';
+import ApiError from '../error/ApiError';
 const auth: Middleware = (req, res, next) => {
 	if (req.method == 'OPTIONS') {
 		return next();
 	}
 	try {
 		const token = req.headers.authorization?.split(' ')[1];
-		if (!token) {
-			return res.status(401).json({ message: 'Authorization error.' });
+		if (token) {
+			const decodedToken = jwt.verify(token, config.server.jwtSecret) as DecodedTokenType;
+			req.token = decodedToken;
+			return next();
 		}
-
-		const decodedToken = jwt.verify(token, config.server.jwtSecret) as DecodedTokenType;
-		req.token = decodedToken;
-		next();
+		next(ApiError.authError());
 	} catch (err) {
-		return res.status(401).json({ message: 'Authorization error.' });
+		if (err instanceof TokenExpiredError) {
+			return next(ApiError.authError('Session token expired. Please login again.'));
+		}
+		return next(ApiError.authError());
 	}
 };
 
