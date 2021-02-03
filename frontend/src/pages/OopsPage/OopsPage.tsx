7e0836b481510f4b3bf7720e8a6b1fb267660c57
puphs <k.x3@mail.ru>
2021-02-03 20:19:47 +0400

    add redirect after login/register in OopsPage

--- frontend/src/pages/OopsPage/OopsPage.tsx
+++ frontend/src/pages/OopsPage/OopsPage.tsx
@@ -1,17 +1,19 @@
 import styles from './OopsPage.module.scss';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useLocation, useParams } from 'react-router-dom';
+import qs from 'query-string';
 
-const NOT_LOGGED_IN = 'not-logged-in';
-const PAGE_NOT_FOUND = 'page-not-found';
+export const NOT_LOGGED_IN = 'not-logged-in';
+export const PAGE_NOT_FOUND = 'page-not-found';
 
 const OopsPage: React.FC = () => {
 	const { oopsReason } = useParams<{ oopsReason: string }>();
+	const { redirectTo } = qs.parse(useLocation().search);
 
 	let ReasonElement = <PageNotFound />;
 
 	switch (oopsReason) {
 		case NOT_LOGGED_IN:
-			ReasonElement = <NotLoggedIn />;
+			ReasonElement = <NotLoggedIn redirectTo={redirectTo?.toString() ?? '/'} />;
 			break;
 	}
 
@@ -36,19 +38,19 @@ const PageNotFound = () => {
 	);
 };
 
-const NotLoggedIn = () => {
+const NotLoggedIn: React.FC<{ redirectTo: string }> = ({ redirectTo }) => {
 	return (
 		<div className={styles.notLoggedIn}>
 			<p className={styles.p}>
 				You are not logged in!{' '}
-				<Link className={styles.reasonLink} to={'/auth/login/?redirectTo=/cart'}>
+				<Link className={styles.reasonLink} to={`/auth/login?redirectTo=${redirectTo}`}>
 					Login!
 				</Link>
 			</p>
 			<p className={styles.p}>or</p>
 			<p className={styles.p}>
 				If you don’t have an account,{' '}
-				<Link className={styles.reasonLink} to={'/auth/register/?redirectTo=/cart'}>
+				<Link className={styles.reasonLink} to={`/auth/register?redirectTo=${redirectTo}`}>
 					Register!
 				</Link>
 			</p>
